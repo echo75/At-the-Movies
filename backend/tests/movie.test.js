3612cf48f2a31d3c3c205741fc72a8eb34998b43
Johan Hedman <johan@hedman.de>
2026-06-10 17:17:17 +0200

    fix the one test-file

--- backend/tests/movie.test.js
+++ backend/tests/movie.test.js
@@ -1,23 +1,28 @@
 const request = require('supertest')
+jest.mock('../src/services/registermail', () => ({
+  sendRegistrationConfirmationEmail: jest.fn().mockResolvedValue(true),
+}))
 const app = require('../src/app')
 const User = require('../src/models/user')
 
 describe('Movie', () => {
   it('Create a new user named Jenny', async () => {
-    const firstname = 'Jenny'
-    const surname = 'Smith'
+    const firstName = 'Jenny'
+    const surName = 'Smith'
+    const email = `jenny.${Date.now()}@example.com`
+    const password = 'Test1234!'
     const expectedOutput = {
-      firstName: firstname,
-      surName: surname,
+      firstName,
+      surName,
       watched: [],
       watch: [],
       reviews: [],
     }
 
-    const actualOutput = await request(app).post('/users').send({ firstname, surname })
+    const actualOutput = await request(app).post('/users').send({ firstName, surName, email, password })
 
-    expect(actualOutput.body.firstname).toBe(expectedOutput.name)
-    expect(actualOutput.body.surname).toBe(expectedOutput.surname)
+    expect(actualOutput.body.firstName).toBe(expectedOutput.firstName)
+    expect(actualOutput.body.surName).toBe(expectedOutput.surName)
     expect(actualOutput.body.watched).toEqual(expectedOutput.watched)
     expect(actualOutput.body.watch).toEqual(expectedOutput.watch)
     expect(actualOutput.body).toMatchObject(expectedOutput)
